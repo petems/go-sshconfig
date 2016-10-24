9c4bfc0173e290759f33f37c0dcdb9af924be939
Jason Mooberry <jason.mooberry@gmail.com>
2016-10-24 11:41:17 -0400

    first

---
+++ example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/emptyinterface/sshconfig"
+)
+
+func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	sshConfigFile := os.ExpandEnv("$HOME/.ssh/config")
+
+	file, err := os.Open(sshConfigFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config, err := sshconfig.Parse(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.Close()
+
+	// modify by reference for existing params
+	// or create a new param and append it to global
+	if param := config.GetParam(sshconfig.VisualHostKeyKeyword); param != nil {
+		param.Args = []string{"yes"}
+	} else {
+		param = sshconfig.NewParam(sshconfig.VisualHostKeyKeyword, []string{"yes"}, []string{"good to see you"})
+		config.Globals = append(config.Globals, param)
+	}
+
+	// atomic write to file to ensure config is preserved in
+	// the event of an error
+	if err := config.WriteToFilepath("./example_config"); err != nil {
+		log.Fatal(err)
+	}
+
+}
